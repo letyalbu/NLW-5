58da28af753824fa35d7300321a4ae01ff1f13c0
Leticia Albuquerque <lety.albu6@gmail.com>
2021-04-21 15:51:57 -0300

    Database: add Users and Messages tables

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import "./database";
-import {routes} from "./routes";
+  import express from "express";
+  import "./database";
+  import {routes} from "./routes";
 
-const app = express();
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
 
-app.use(routes);
+  app.use(routes);
 
-app.listen(3333, () => console.log("Server is running on port 3333"));
+  app.listen(3333, () => console.log("Server is running on port 3333"));
