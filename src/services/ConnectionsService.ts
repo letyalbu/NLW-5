fe6198a025f288fc601e8d7d8996f5ad02118f71
Leticia Albuquerque <lety.albu6@gmail.com>
2021-04-24 17:54:14 -0300

    connection between client and admin made with WB

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -10,31 +10,58 @@ interface IConnectionCreate{
 }
 
 class ConnectionsService {
-  private connectionsService: Repository<Connection>
+  private connectionsRepository: Repository<Connection>
 
   constructor(){
-    this.connectionsService = getCustomRepository(ConnectionsRepository);
+    this.connectionsRepository = getCustomRepository(ConnectionsRepository);
   }
 
   async create({ admin_id, user_id, socket_id, id } : IConnectionCreate){
-    const connection = this.connectionsService.create({
+    const connection = this.connectionsRepository.create({
       admin_id,
       user_id,
       socket_id,
       id,
     })
 
-    await this.connectionsService.save(connection);
+    await this.connectionsRepository.save(connection);
 
     return connection;
   }
 
   async findByUserId( user_id: string ){
-    const connection = this.connectionsService.findOne({
+    const connection = this.connectionsRepository.findOne({
       user_id,
     })
 
     return connection;
+  }
+
+  async findAllWithoutAdmin(){
+    const connections = await this.connectionsRepository.find({
+      where: { admin_id: null },
+      relations: ["user"],
+    })
+    return connections;
+  }
+
+  async findBySocketID( socket_id: string ){
+    const connection = await this.connectionsRepository.findOne({
+      socket_id,
+    })
+
+    return connection;
+  }
+
+  async updateAdminId(user_id: string, admin_id: string){
+    await this.connectionsRepository
+    .createQueryBuilder()
+    .update(Connection)
+    .set({admin_id})
+    .where( "user_id = :user_id", {
+      user_id,
+    })
+    .execute();
   }
 }
 
